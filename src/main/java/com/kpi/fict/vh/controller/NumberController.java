53c109a45f77970924697c1057e699d673b8b5e5
VHerashchenko <vadim.gerashchenko@gmail.com>
2021-08-26 17:32:56 +0300

    NumberController clean processUser

--- src/main/java/com/kpi/fict/vh/controller/NumberController.java
+++ src/main/java/com/kpi/fict/vh/controller/NumberController.java
@@ -24,13 +24,27 @@ public class NumberController {
     public void processUser(){
         Scanner scanner = new Scanner(System.in);
 
+        String borderControl = setBorderQuestion(scanner);
+
+        setBorderByBorderString(borderControl, scanner);
+
+        guessGame(scanner);
+
+        numberView.printLog(numberContainer.getLogList());
+
+    }
+
+    private String setBorderQuestion(Scanner scanner){
         numberView.printMessage("Do you want to change default border? y/n");
         String borderControl = scanner.nextLine();
         while( ! (borderControl.equalsIgnoreCase("y") || borderControl.equalsIgnoreCase("n"))) {
             numberView.printMessage(NumberView.WRONG_INPUT_INT_DATA_REPEAT);
             borderControl = scanner.nextLine();
         }
+        return borderControl;
+    }
 
+    private void setBorderByBorderString(String borderControl, Scanner scanner){
         if(borderControl.equalsIgnoreCase("y")) {
             int minValue = borderPatrol(NumberContainer.DEFAULT_MIN_VALUE, NumberContainer.DEFAULT_MAX_VALUE, scanner);
             int maxValue = borderPatrol(minValue, NumberContainer.DEFAULT_MAX_VALUE, scanner);
@@ -41,7 +55,9 @@ public class NumberController {
         else {
             numberService.setNumberIntByMaxMinValues(numberContainer);
         }
+    }
 
+    private void guessGame(Scanner scanner){
         int enteredNumber = borderPatrol(numberContainer.getMinInt(), numberContainer.getMaxInt(), scanner);
         while (! numberService.isNumberRight(numberContainer, enteredNumber)){
 
@@ -55,9 +71,6 @@ public class NumberController {
             }
             enteredNumber = borderPatrol(numberContainer.getMinInt(), numberContainer.getMaxInt(), scanner);
         }
-
-        numberView.printLog(numberContainer.getLogList());
-
     }
 
     private int borderPatrol(int minValue, int maxValue, Scanner scanner){
